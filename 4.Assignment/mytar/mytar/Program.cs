34b0ab13a6be5282f00bf4eab9e14df54a8ec8f2
ernestkrom <ernestkrom@gmail.com>
2019-01-09 17:38:51 +0100

    First minor implementation

--- 4.Assignment/mytar/mytar/Program.cs
+++ 4.Assignment/mytar/mytar/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace mytar
 {
@@ -6,28 +7,45 @@ namespace mytar
     {
         public static void Main(string[] args)
         {
+            string ops, dest, src;
+
+            try
+            {
+                ops = args[0];
+                dest = args[1];
+                src = Directory.GetCurrentDirectory();
+
+                switch (ops)
+                {
+                    case "--store":
+                        string contents="";
+                        foreach (string file in Directory.EnumerateFiles(src, "*.bin"))
+                        {
+                            Console.WriteLine(file);
+                            contents = File.ReadAllText(file);
+                        }
+                        //Console.WriteLine((contents));
+                        break;
+                    case "--restore":
+                        break;
+                    default:
+                        invokeManual();
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                invokeManual();
+            }
+        }
+
+        private static void invokeManual()
+        {
             // @ indicates a verbatim string literal in this instance
-            string man = @"USAGE: mytar [--store|--restore|--help] dest src {src}
+            Console.WriteLine(@"USAGE: mytar [--store|--restore|--help] dest src {src}
     --store:    packs all src into [dest]
     --restore:  unpacks all files stored in [dest] into the current directory
-    --help:     this screen";
-
-            if (args.Length != 0)
-            {
-                try
-                {
-                    string argument = args[0];
-                    string destination = args[1];
-                    string source = args[2];
-                }
-                
-                // input for archiving is current directory
-                // unpack into current directory --> src for extraction can be anywhere
-            }
-            else
-            {
-                Console.WriteLine(man);
-            }
+    --help:     this screen");
         }
     }
 }
