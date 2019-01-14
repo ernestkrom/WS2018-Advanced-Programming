dc0c47cc4f03f0394e98f1fd51102094c5b16615
ernestkrom <ernestkrom@gmail.com>
2019-01-14 21:40:32 +0100

    First minor implementation

--- 4.Assignment/mytar/mytar/Program.cs
+++ 4.Assignment/mytar/mytar/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace mytar
 {
@@ -19,12 +21,12 @@ namespace mytar
                 {
                     case "--store":
                         string contents="";
-                        foreach (string file in Directory.EnumerateFiles(src, "*.bin"))
+                        foreach (string file in Directory.EnumerateFiles(src, "*.*"))
                         {
-                            Console.WriteLine(file);
-                            contents = File.ReadAllText(file);
+                            contents += StringToBinary(File.ReadAllText(file));
                         }
-                        //Console.WriteLine((contents));
+                        Console.WriteLine((contents));
+                        Console.WriteLine((BinaryToString(contents)));
                         break;
                     case "--restore":
                         break;
@@ -35,10 +37,38 @@ namespace mytar
             }
             catch (Exception e)
             {
+                Console.Write(e);
                 invokeManual();
             }
         }
 
+        /// <summary>
+        /// https://www.fluxbytes.com/csharp/convert-string-to-binary-and-binary-to-string-in-c/
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static string StringToBinary(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+ 
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+        
+        public static string BinaryToString(string data)
+        {
+            List<Byte> byteList = new List<Byte>();
+ 
+            for (int i = 0; i < data.Length; i += 8)
+            {
+                byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
+            }
+            return Encoding.ASCII.GetString(byteList.ToArray());
+        }
+        
         private static void invokeManual()
         {
             // @ indicates a verbatim string literal in this instance
